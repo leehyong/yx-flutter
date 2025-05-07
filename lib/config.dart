db7da94a4c6399eeab9660b48f0bb44edc2dfd5d
leesen <lxy1148545418@163.com>
2025-05-07 09:03:20 +0800

    使用内网穿透工具 ngrok， 来代理本地服务，从而使手机端可以访问

--- lib/config.dart
+++ lib/config.dart
@@ -4,8 +4,10 @@ import 'package:get/get_utils/get_utils.dart';
 const publicKey =
     '046fa99b946d5ae8559287057264385b957218ceb09bc3dbff89c0d8fa1c84a73748c9969f2626e8434772972e7188c1aea2db6ed545f3ad0361242e3805141622';
 // const apiServer = "https://flybook.gzdx.com.cn:30666";
+// 使用内网穿透工具 ngrok， 来代理本地服务，从而使手机端可以访问
 final apiServer =
-    GetPlatform.isMobile ? "https://10.0.2.2:18012" : "https://www.yoo.com:18012";
+    // GetPlatform.isMobile ? "https://10.0.2.2:18012" : "https://www.yoo.com:18012";
+    GetPlatform.isMobile ? "https://deeply-included-polecat.ngrok-free.app" : "https://www.yoo.com:18012";
 
 const xxCusHeaderOfAccessToken = 'xx-cus-token';
 const accessTokenStr = 'access-token';
